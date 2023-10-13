bfd94f13734f9fc500d5f0fd2345a1b4e2277336
sangmaojung520 <142375458+sangmaojung520@users.noreply.github.com>
2023-10-13 09:01:16 +0800

    first version

---
+++ main.f90
@@ -0,0 +1,26 @@
+program collatz_sequence
+  implicit none
+  real :: i
+  integer :: n
+
+  write(*,*) "Enter a positive integer: "
+  read(*,*) i
+
+  if (i - int(i) == 0 .and. i > 0) then
+    n = int(i)
+    if (n > 0) then
+      do while (n /= 1)
+        if (mod(n, 2) == 0) then
+          n = n / 2
+        else
+          n = 3 * n + 1
+        end if
+        write(*,*) " ", n
+      end do
+      write(*,*)
+    end if
+  else
+    write(*,*) "Please Enter a positive integer."
+  end if
+
+end program collatz_sequence
